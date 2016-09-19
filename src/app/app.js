2f0783c1ab11d61c91d66ecef9dba4fbba61adcf
Joseph Parkton <creekhippybear@gmail.com>
2016-09-19 11:04:30 -0700

    pushing first run

---
+++ src/app/app.js
@@ -0,0 +1,51 @@
+angular.module('appoints', [
+  'ngRoute',
+  'ui.bootstrap.datetimepicker',
+  'appoints.directives',
+  'appoints.flash',
+  'appoints.usersession',
+  'appoints.authinterceptor',
+  'appoints.home',
+  'appoints.login',
+  'appoints.appointments',
+  'appoints-client-templates'
+])
+
+// allow DI for use in controllers, unit tests
+.constant('_', window._)
+.constant('moment', window.moment)
+
+.config(function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+})
+
+.controller('AppCtrl', function AppController ($scope, $location, usersession) {
+  var defaultPageTitle = 'Appoints';
+
+  $scope.pageTitle = defaultPageTitle;
+
+  $scope.$on('$routeChangeSuccess', function (ev, currentRoute) {
+    $scope.pageTitle = currentRoute.title || defaultPageTitle;
+  });
+
+  $scope.user = usersession.current;
+
+  $scope.routeIs = function(routeName) {
+    return $location.path() === routeName;
+  };
+
+  $scope.logout = function () {
+    usersession.logout();
+    $location.url('/');
+  };
+
+  $scope.$on('event:loggedin', function (ev, currentSession) {
+    $scope.user = currentSession;
+  });
+
+  $scope.$on('event:loggedout', function (ev, currentSession) {
+    $scope.user = currentSession;
+  });
+
+});
---
+++ src/app/home/home.js
@@ -0,0 +1,17 @@
+angular.module('appoints.home', [
+  'appoints.config',
+  'ngRoute'
+])
+
+.config(function config($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'home/home.html',
+      controller: 'HomeCtrl',
+      title: 'Home'
+    });
+})
+
+.controller('HomeCtrl', function HomeController($scope, appVersion) {
+  $scope.version = appVersion;
+});
